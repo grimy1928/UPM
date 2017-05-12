fbce818b24efa2f5222a41d216a28a98221bdcf3
Daniel <dannutz_mic@yahoo.com>
2017-05-12 13:21:54 +0300

    Delete add_to_buildprop.sh

--- UPM-Modified by Dany00/add_to_buildprop.sh
+++
@@ -1,5 +0,0 @@
-#!/sbin/sh
-#By Jeeko
-busybox echo "" >> /system/build.prop
-busybox echo "# Ultimate Performance Mod Tweaks" >> /system/build.prop
-echo "setprop persist.sys.use_dithering 1 build.prop"
